2e6c882e8d961a857e2d2dbd0198f111b080ff76
vaheedk <91840523+vaheedk@users.noreply.github.com>
2021-10-03 22:19:53 +0330

    Update Layout.js

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -4,8 +4,8 @@ import classes from './Layout.module.css';
 function Layout(props) {
   return (
     <div>
-      <MainNavigation /> // the header nav bar goes here ( you can also delete it )
-      <main className={classes.main}>{props.children}</main> // all the tags an other components goes here since we use Layout tag as a wrapping tag, this means that all other content between open/close tags of Layout will be found as props.children. 
+      <MainNavigation /> 
+      <main className={classes.main}>{props.children}</main> 
     </div>
   );
 }
